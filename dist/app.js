2d3fcf1012f759048b1d3df2819d33af3c8d941d
Hasibul Islam <hasibulislam@HasibsMdioM2Max.fritz.box>
2024-07-23 02:49:53 +0200

    api tested in postman

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
+// parser
+app.use(express_1.default.json()); //without this req is not possible to read sent as  json data. 
+app.use(express_1.default.text());
+// middlewre 
+const logger = (req, res, next) => {
+    console.log('url is ', req.url, 'method is ', req.method, 'host name is ', req.hostname);
+    next();
+};
+app.get('/', logger, (req, res) => {
     res.send('Hello World!');
+});
+app.post('/', logger, (req, res) => {
+    console.log(req.body);
+    res.send('Post is done here');
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,33 @@
 
 
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+
+// parser
+app.use(express.json()) //without this req is not possible to read sent as  json data. 
+app.use(express.text()) 
+
+// middlewre 
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log('url is ', req.url, 'method is ', req.method, 'host name is ',req.hostname);
+    next();
+};
+
+
+
+app.get('/', logger, (req: Request, res: Response) => {
     res.send('Hello World!')
+});
+
+
+app.post('/', logger,  (req: Request, res: Response) => {
+    console.log(req.body);
+    res.send('Post is done here')
 })
+
 
 
 
